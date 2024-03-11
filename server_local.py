8a93a802e5fffac75c668fa75d72f2904cfd3e53
Kimio Nishiura <kimio.nishiura@me.com>
2024-03-11 17:53:48 +0100

    add local and online server

---
+++ server_local.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# read data from excel
+data = pd.read_excel('TeslaNumbers.xlsx')
+# convert data to json format
+earnings = data.to_dict()
+
+# route for data
+@app.route('/tesla_earnings', methods=['GET'])
+def get_data():
+    return jsonify(earnings)
+
+if __name__ == '__main__':
+    # Listen on all network interfaces
+    app.run(host='0.0.0.0', port=5001, debug=True)
+    
